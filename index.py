711c071dcea3406fcfb3af27c6d8993758dcb129
Никита <mnd1work@gmail.com>
2022-12-13 01:15:38 +0400

    commit 13.12.22 (2)

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from telethon.sync import TelegramClient
 from telethon import functions, types
 from random import randrange
+from time import sleep
 
 from config import NAME, API_ID, API_HASH
 
@@ -9,22 +10,28 @@ f = open("base.txt")
 fd = f.read()
 parse = fd.split('\n')
 
+i = 0
+a = 0
+limit = 300
 
 with TelegramClient(NAME, API_ID, API_HASH) as client:
 
-    i = 0
-    a = 100
+    while (a < limit):
 
-    while (i < 50):
+        while (i < 50):
 
-        result = client(functions.contacts.ImportContactsRequest(
-            contacts=[types.InputPhoneContact(
-                client_id = randrange(-2**63, 2**63),
-                phone = parse[i],
-                first_name = f'Base {a + i}',
-                last_name = ''
-            )]
-        ))
+            result = client(functions.contacts.ImportContactsRequest(
+                contacts=[types.InputPhoneContact(
+                    client_id = randrange(-2**63, 2**63),
+                    phone = parse[a + i],
+                    first_name = f'Base {a + i}',
+                    last_name = ''
+                )]
+            ))
 
-        i += 1
-        print(f'Base {i}')
+            i += 1
+            print(f'Base {a + i}')
+
+        i = 0
+        a += 50
+        sleep(320)
